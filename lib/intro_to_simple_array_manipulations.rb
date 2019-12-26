0c264b12de90fbf88702074744a5dc9a13f532ea
Mitchell T Alton <mitchell.alton@live.com>
2019-12-26 21:48:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(instructors, steven)
   instructors.delete("Steven")
 end 
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, 2)
   famous_robots.delete_at(2)
 end 
